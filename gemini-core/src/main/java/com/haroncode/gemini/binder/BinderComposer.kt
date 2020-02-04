4b76474f8f15d2216282f1a396a54176628d5f17
NikitaZaltsman <zaltsman.nikita@gmail.com>
2020-02-04 08:56:38 +0700

    Add binder composer

---
+++ gemini-core/src/main/java/com/haroncode/gemini/binder/BinderComposer.kt
@@ -0,0 +1,21 @@
+package com.haroncode.gemini.binder
+
+import com.haroncode.gemini.core.StoreView
+import java.util.concurrent.CopyOnWriteArraySet
+
+class BinderComposer<Action : Any, State : Any>(
+        vararg binders: ViewBinder<Action, State>
+) : ViewBinder<Action, State> {
+
+    private val binderSet = CopyOnWriteArraySet<ViewBinder<Action, State>>()
+            .apply { addAll(binders) }
+
+    override fun bindView(storeView: StoreView<Action, State>) = binderSet
+            .forEach { binder -> binder.bindView(storeView) }
+
+    override fun unbindView() = binderSet
+            .forEach { binder -> binder.unbindView() }
+
+    fun addBinder(binder: ViewBinder<Action, State>) = binderSet.add(binder)
+
+}
