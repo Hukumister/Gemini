e2eafba62a54ce985b8b3900f254ace549df9dc1
NikitaZaltsman <zaltsman.nikita@gmail.com>
2020-02-14 21:11:23 +0700

    fix view model module

--- gemini-viewmodel-binder/src/main/java/com/haroncode/gemini/android/binder/fragment/MviFragment.kt
+++ gemini-viewmodel-binder/src/main/java/com/haroncode/gemini/android/binder/fragment/MviFragment.kt
@@ -2,18 +2,18 @@ package com.haroncode.gemini.android.binder.fragment
 
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
-import com.haroncode.gemini.core.StoreView
 import com.haroncode.gemini.android.binder.viewmodel.ViewModelBinder
+import com.haroncode.gemini.core.StoreView
 import io.reactivex.processors.PublishProcessor
 import org.reactivestreams.Subscriber
 
-abstract class MviFragment<Action : Any, State : Any> @JvmOverloads constructor(
+abstract class MviFragment<Action : Any, ViewState : Any> @JvmOverloads constructor(
     @LayoutRes contentLayoutId: Int = 0
-) : Fragment(contentLayoutId), StoreView<Action, State> {
+) : Fragment(contentLayoutId), StoreView<Action, ViewState> {
 
     private val source = PublishProcessor.create<Action>()
 
-    abstract val viewModelBinder: ViewModelBinder<Action, *, State, *>
+    abstract val viewModelBinder: ViewModelBinder<Action, *, ViewState, *>
 
     override fun onStart() {
         super.onStart()
@@ -25,11 +25,11 @@ abstract class MviFragment<Action : Any, State : Any> @JvmOverloads constructor(
         viewModelBinder.unbindView()
     }
 
-    abstract fun onViewStateChanged(state: State)
+    abstract fun onViewStateChanged(state: ViewState)
 
     fun postAction(action: Action) = source.onNext(action)
 
-    final override fun accept(state: State) = onViewStateChanged(state)
+    final override fun accept(state: ViewState) = onViewStateChanged(state)
 
     final override fun subscribe(subscriber: Subscriber<in Action>) = source.subscribe(source)
 
