aa9807e1bff7f0a21b254b06c8603126eb068cd8
NikitaZaltsman <zaltsman.nikita@gmail.com>
2020-03-02 23:29:17 +0700

    remove connection binder

--- gemini-viewmodel-binder/src/main/java/com/haroncode/gemini/android/binder/viewmodel/ViewModelConnectionBinder.kt
+++ gemini-viewmodel-binder/src/main/java/com/haroncode/gemini/android/binder/viewmodel/ViewModelConnectionBinder.kt
@@ -1,29 +1,21 @@
 package com.haroncode.gemini.android.binder.viewmodel
 
-import com.haroncode.gemini.core.ConnectionBinder
 import com.haroncode.gemini.core.ConnectionRule
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.addTo
 
-class ViewModelConnectionBinder : ConnectionBinder {
+class ViewModelConnectionBinder : Disposable {
 
     private val connections = CompositeDisposable()
-    private val retainConnections = CompositeDisposable()
 
-    override fun bind(connectionRule: ConnectionRule) {
-        if (connectionRule.isRetain) {
-            connectionRule.connect().addTo(retainConnections)
-        } else {
-            connectionRule.connect().addTo(connections)
-        }
+    fun bind(connectionRule: ConnectionRule) {
+        connectionRule.connect().addTo(connections)
     }
 
     fun clear() = connections.clear()
 
     override fun isDisposed() = connections.isDisposed
 
-    override fun dispose() {
-        connections.dispose()
-        retainConnections.dispose()
-    }
+    override fun dispose() = connections.dispose()
 }
