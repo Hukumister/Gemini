2a8e3f86747f49907154c93aec949e373cda0c70
NikitaZaltsman <zaltsman.nikita@gmail.com>
2020-03-02 23:28:26 +0700

    refactoring package of event listener

--- gemini-core/src/main/java/com/haroncode/gemini/connection/EventListenerConnection.kt
+++ gemini-core/src/main/java/com/haroncode/gemini/connection/EventListenerConnection.kt
@@ -1,7 +1,7 @@
-package com.haroncode.gemini.event
+package com.haroncode.gemini.connection
 
-import com.haroncode.gemini.connection.BaseConnectionRule
 import com.haroncode.gemini.connection.dsl.identityFlowableTransformer
+import com.haroncode.gemini.core.EventListener
 import io.reactivex.functions.Consumer
 import org.reactivestreams.Publisher
 
