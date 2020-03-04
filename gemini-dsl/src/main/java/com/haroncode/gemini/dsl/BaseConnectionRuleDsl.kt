21082539c53b9a5c1512755fb3875d901ced2f19
NikitaZaltsman <zaltsman.nikita@gmail.com>
2020-03-04 13:55:12 +0700

    move dsl connection rule to dsl module

--- gemini-dsl/src/main/java/com/haroncode/gemini/dsl/BaseConnectionRuleDsl.kt
+++ gemini-dsl/src/main/java/com/haroncode/gemini/dsl/BaseConnectionRuleDsl.kt
@@ -1,6 +1,12 @@
-package com.haroncode.gemini.connection.dsl
+@file:Suppress("NOTHING_TO_INLINE")
+
+package com.haroncode.gemini.dsl
 
 import com.haroncode.gemini.connection.BaseConnectionRule
+import com.haroncode.gemini.connection.EventListenerConnection
+import com.haroncode.gemini.core.EventListener
+import com.haroncode.gemini.core.Store
+import com.haroncode.gemini.core.StoreView
 import io.reactivex.Flowable
 import io.reactivex.FlowableTransformer
 import io.reactivex.Scheduler
@@ -8,6 +14,8 @@ import io.reactivex.functions.Consumer
 import org.reactivestreams.Publisher
 
 typealias Transformer<In, Out> = (Flowable<In>) -> Flowable<Out>
+
+typealias Mapper<In, Out> = (In) -> Out
 
 private val IDENTITY_TRANSFORMER: Transformer<Any, Any> = { stream -> stream }
 
@@ -19,6 +27,12 @@ fun <T : Any> identityFlowableTransformer() = IDENTITY_FLOWABLE_TRANSFORMER as F
 @Suppress("UNCHECKED_CAST")
 fun <T : Any> identityTransformer() = IDENTITY_TRANSFORMER as Transformer<T, T>
 
+inline infix fun <T : Any> Publisher<T>.to(consumer: Consumer<T>) = BaseConnectionRule(
+    consumer = consumer,
+    publisher = this,
+    transformer = identityFlowableTransformer()
+)
+
 inline infix fun <Out : Any, In : Any> Pair<Publisher<Out>, Consumer<In>>.with(
     crossinline transformer: Transformer<Out, In>
 ) = BaseConnectionRule(
@@ -27,12 +41,25 @@ inline infix fun <Out : Any, In : Any> Pair<Publisher<Out>, Consumer<In>>.with(
     transformer = FlowableTransformer { stream -> transformer.invoke(stream) }
 )
 
-@Suppress("NOTHING_TO_INLINE")
-inline fun <T : Any> schedulerTransformer(
-    scheduler: Scheduler
-): Transformer<T, T> = { input -> input.observeOn(scheduler) }
+inline infix fun <Out : Any, In : Any> Pair<Publisher<Out>, Consumer<In>>.mapper(
+    crossinline mapper: Mapper<Out, In>
+) = BaseConnectionRule(
+    consumer = this.second,
+    publisher = this.first,
+    transformer = FlowableTransformer { stream -> stream.map { input -> mapper.invoke(input) } }
+)
 
-@Suppress("NOTHING_TO_INLINE")
+inline infix fun <T : Any, R : Any> BaseConnectionRule<T, R>.observeOn(
+    scheduler: Scheduler
+): BaseConnectionRule<T, R> = BaseConnectionRule(
+    consumer = this.consumer,
+    publisher = this.publisher,
+    transformer = FlowableTransformer { stream ->
+        Flowable.fromPublisher(this.transformer.apply(stream))
+            .observeOn(scheduler)
+    }
+)
+
 inline infix fun <T : Any> BaseConnectionRule<T, T>.decorate(
     noinline transformer: Transformer<T, T>
 ): BaseConnectionRule<T, T> = BaseConnectionRule(
@@ -42,4 +69,11 @@ inline infix fun <T : Any> BaseConnectionRule<T, T>.decorate(
         Flowable.fromPublisher(this.transformer.apply(stream))
             .compose(transformer)
     }
+)
+
+inline infix fun <Event : Any> Store<*, *, Event>.eventTo(
+    eventListener: EventListener<Event>
+) = EventListenerConnection(
+    eventPublisher = this.eventSource,
+    eventListener = eventListener
 )
