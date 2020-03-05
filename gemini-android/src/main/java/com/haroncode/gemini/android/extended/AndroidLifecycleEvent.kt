4b9ad1ea7f90dd5e131e264b23c8f730823b45b2
NikitaZaltsman <zaltsman.nikita@gmail.com>
2020-03-05 10:56:40 +0700

    add event extended lifecycle observer

---
+++ gemini-android/src/main/java/com/haroncode/gemini/android/extended/AndroidLifecycleEvent.kt
@@ -0,0 +1,38 @@
+package com.haroncode.gemini.android.extended
+
+enum class AndroidLifecycleEvent {
+    /**
+     * Constant for onCreate event of the {@link LifecycleOwner}.
+     */
+    ON_CREATE,
+
+    /**
+     * Constant for onStart event of the {@link LifecycleOwner}.
+     */
+    ON_START,
+
+    /**
+     * Constant for onResume event of the {@link LifecycleOwner}.
+     */
+    ON_RESUME,
+
+    /**
+     * Constant for onPause event of the {@link LifecycleOwner}.
+     */
+    ON_PAUSE,
+
+    /**
+     * Constant for onStop event of the {@link LifecycleOwner}.
+     */
+    ON_STOP,
+
+    /**
+     * Constant for onDestroy event of the {@link LifecycleOwner}.
+     */
+    ON_DESTROY,
+
+    /**
+     * Constant for real destroy event (it means destroy not after rotation) of the {@link LifecycleOwner}.
+     */
+    ON_REAL_REMOVING
+}
