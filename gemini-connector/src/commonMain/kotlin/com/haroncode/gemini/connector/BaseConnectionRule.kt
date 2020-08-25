9d687f37301e3616c97a97cd6eca1b2eb988849e
Nikita Zaltsman <zaltsman.nikita@gmail.com>
2020-08-26 00:26:25 +0700

    update: add dsl factory function to base connection rule

--- gemini-connector/src/commonMain/kotlin/com/haroncode/gemini/connector/BaseConnectionRule.kt
+++ gemini-connector/src/commonMain/kotlin/com/haroncode/gemini/connector/BaseConnectionRule.kt
@@ -1,6 +1,7 @@
 package com.haroncode.gemini.connector
 
 import com.haroncode.gemini.StoreEventListener
+import com.haroncode.gemini.StoreView
 import com.haroncode.gemini.element.Store
 import com.haroncode.gemini.functional.Consumer
 import kotlinx.coroutines.cancel
@@ -34,10 +35,53 @@ open class BaseConnectionRule<Out : Any, In : Any>(
         .collect { consumer.accept(it) }
 }
 
+
 /**
  *
  */
-infix fun <T : Any> Flow<T>.connectTo(
+infix fun <Event : Any> Store<*, *, Event>.bindEventTo(
+    eventListener: StoreEventListener<Event>
+) = eventFlow bindTo eventListener
+
+/**
+ *
+ */
+infix fun <Event : Any, ViewEvent : Any> Store<*, *, Event>.bindEventTo(
+    eventListener: StoreEventListener<ViewEvent>
+) = stateFlow bindTo eventListener
+
+/**
+ *
+ */
+infix fun <State : Any> Store<*, State, *>.bindStateTo(
+    consumer: Consumer<State>
+) = stateFlow bindTo consumer
+
+/**
+ *
+ */
+infix fun <State : Any, ViewState : Any> Store<*, State, *>.bindStateTo(
+    consumer: Consumer<ViewState>
+) = stateFlow bindTo consumer
+
+/**
+ *
+ */
+infix fun <Action : Any> StoreView<Action, *>.bindActionTo(
+    consumer: Consumer<Action>
+) = actionFlow bindTo consumer
+
+/**
+ *
+ */
+infix fun <ViewAction : Any, Action : Any> StoreView<ViewAction, *>.bindActionTo(
+    consumer: Consumer<Action>
+) = actionFlow bindTo consumer
+
+/**
+ *
+ */
+infix fun <T : Any> Flow<T>.bindTo(
     consumer: Consumer<T>
 ): BaseConnectionRule<T, T> = BaseConnectionRule(
     consumer = consumer,
@@ -48,21 +92,21 @@ infix fun <T : Any> Flow<T>.connectTo(
 /**
  *
  */
-infix fun <Out : Any, In : Any> Flow<Out>.connectTo(
+infix fun <Out : Any, In : Any> Flow<Out>.bindTo(
     consumer: Consumer<In>
 ): Pair<Flow<Out>, Consumer<In>> = this to consumer
 
 /**
  *
  */
-infix fun <Out : Any, In : Any> Flow<Out>.connectEventTo(
+infix fun <Out : Any, In : Any> Flow<Out>.bindTo(
     eventListener: StoreEventListener<In>
 ): Pair<Flow<Out>, Consumer<In>> = this to Consumer { event -> eventListener.onEvent(event) }
 
 /**
  *
  */
-infix fun <T : Any> Flow<T>.connectEventTo(
+infix fun <T : Any> Flow<T>.bindTo(
     eventListener: StoreEventListener<T>
 ): BaseConnectionRule<T, T> = BaseConnectionRule(
     consumer = { event -> eventListener.onEvent(event) },
