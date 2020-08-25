4806462a1c1eef9e469534a5f808f9d69e7127b7
Nikita Zaltsman <zaltsman.nikita@gmail.com>
2020-08-25 18:11:17 +0700

    update: merge delegate connection rules factory and dsl builder

--- gemini-android/src/main/java/com/haroncode/gemini/android/connector/DelegateConnectionRulesFactory.kt
+++ gemini-android/src/main/java/com/haroncode/gemini/android/connector/DelegateConnectionRulesFactory.kt
@@ -1,8 +1,11 @@
 package com.haroncode.gemini.android.connector
 
 import androidx.lifecycle.LifecycleOwner
+import com.haroncode.gemini.connector.AutoCancelStoreRule
+import com.haroncode.gemini.connector.BaseConnectionRule
 import com.haroncode.gemini.connector.ConnectionRule
 import com.haroncode.gemini.connector.ConnectionRulesFactory
+import com.haroncode.gemini.element.Store
 
 /**
  * @author HaronCode
@@ -13,4 +16,30 @@ abstract class DelegateConnectionRulesFactory<T : LifecycleOwner> : ConnectionRu
     abstract val connectionRulesFactory: ConnectionRulesFactory<T>
 
     override fun create(param: T): Collection<ConnectionRule> = connectionRulesFactory.create(param)
+}
+
+inline fun <reified T : Any> connectionRulesFactory(
+    crossinline factory: ConnectionRuleListBuilder.(param: T) -> Unit
+): ConnectionRulesFactory<T> = ConnectionRulesFactory { param ->
+    val builder = ConnectionRuleListBuilder()
+    builder.factory(param)
+    builder.build()
+}
+
+class ConnectionRuleListBuilder {
+
+    private val connectionRules = mutableListOf<ConnectionRule>()
+
+    fun rule(connectionRule: () -> BaseConnectionRule<*, *>) {
+        connectionRule.invoke()
+            .let(connectionRules::add)
+    }
+
+    fun autoCancel(storeProvider: () -> Store<*, *, *>) {
+        storeProvider.invoke()
+            .let(::AutoCancelStoreRule)
+            .let(connectionRules::add)
+    }
+
+    fun build() = connectionRules.toList()
 }
