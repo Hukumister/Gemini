151024d77fde9026ba248677aa1aee7d5070081a
Nikita Zaltsman <zaltsman.nikita@gmail.com>
2020-08-26 11:33:35 +0700

    update: fix review issues

--- gemini-connector/src/commonMain/kotlin/com/haroncode/gemini/connector/Transformer.kt
+++ gemini-connector/src/commonMain/kotlin/com/haroncode/gemini/connector/Transformer.kt
@@ -11,7 +11,7 @@ fun interface Transformer<in In, out Out> {
     fun transform(input: Flow<In>): Flow<Out>
 }
 
-fun <T> identityTransformer() = IdentityTransformer as Transformer<T, T>
+fun <T : Any> identityTransformer() = IdentityTransformer as Transformer<T, T>
 
 object IdentityTransformer : Transformer<Any, Any> {
     override fun transform(input: Flow<Any>): Flow<Any> = input
