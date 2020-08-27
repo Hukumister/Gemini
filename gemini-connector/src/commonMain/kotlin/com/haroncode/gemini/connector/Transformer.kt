ba821f99929a8446ae8476600647036969abec49
Dias Kinayatov <kdk96s@gmail.com>
2020-08-27 16:19:42 +0700

    suppress identityTransformer unchecked cast

--- gemini-connector/src/commonMain/kotlin/com/haroncode/gemini/connector/Transformer.kt
+++ gemini-connector/src/commonMain/kotlin/com/haroncode/gemini/connector/Transformer.kt
@@ -11,6 +11,7 @@ fun interface Transformer<in In, out Out> {
     fun transform(input: Flow<In>): Flow<Out>
 }
 
+@Suppress("UNCHECKED_CAST")
 fun <T : Any> identityTransformer() = IdentityTransformer as Transformer<T, T>
 
 object IdentityTransformer : Transformer<Any, Any> {
