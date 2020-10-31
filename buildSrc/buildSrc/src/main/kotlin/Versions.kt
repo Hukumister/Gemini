1aa9702373ae33093849f8c291d1461fa20e20d5
Dias Kinayatov <kdk96s@gmail.com>
2020-10-31 23:30:52 +0700

    update androidx versions

--- buildSrc/buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/buildSrc/src/main/kotlin/Versions.kt
@@ -16,9 +16,9 @@ object Versions {
     object androidx {
         const val appCompat = "1.2.0"
         const val lifecycle = "2.2.0"
-        const val core = "1.3.1"
-        const val material = "1.2.0"
-        const val constraintLayout = "2.0.0"
+        const val core = "1.3.2"
+        const val material = "1.2.1"
+        const val constraintLayout = "2.0.4"
     }
 
     const val toothpick = "3.1.0"
